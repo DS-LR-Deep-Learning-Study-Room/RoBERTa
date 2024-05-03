1d08c4a6d5a4b0e20f0cdf0173ad7cae5fabec9a
Junnos <junnos.dev@gmail.com>
2024-05-03 16:44:12 +0900

    Add const file and replaced static strings

--- __main__.py
+++ __main__.py
@@ -1,18 +1,29 @@
+import logging
+
 from transformers import AutoTokenizer
 from torch.utils.data import DataLoader
 
 from .dataset.dataset import QuestionDataset
+from .const import (
+    PRETRAIN_URL,
+    TRAIN_SET,
+    TEST_SET,
+    VALID_SET
+)
+
+_LOGGER = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    tokenizer = AutoTokenizer.from_pretrained("FacebookAI/xlm-roberta-base")
+    tokenizer = AutoTokenizer.from_pretrained(PRETRAIN_URL)
 
-    train_dataset = QuestionDataset(
-        filename="train.parquet",
-        tokenizer=tokenizer
-    )
+    train_dataset = QuestionDataset(filename=TEST_SET, tokenizer=tokenizer)
+    valid_dataset = QuestionDataset(filename=VALID_SET, tokenizer=tokenizer)
+    test_dataset = QuestionDataset(filename=TEST_SET, tokenizer=tokenizer)
 
-    train_dataloader = DataLoader(dataset=train_dataset)
-    a, b = next(iter(train_dataloader))
+    train_loader = DataLoader(train_dataset)
+    valid_loader = DataLoader(valid_dataset)
+    test_loader = DataLoader(test_dataset)
+    a, b = next(iter(train_loader))
 
     print(a)
     print(b)
---
+++ const.py
@@ -0,0 +1,7 @@
+### URL
+PRETRAIN_URL = "FacebookAI/xlm-roberta-base"
+
+### Dataset
+TRAIN_SET = "train.parquet"
+TEST_SET = "test.parquet"
+VALID_SET = "valid.parquet"
