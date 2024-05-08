379c6e05f1b58411d7d3ad76d6b295f9b4c60e16
SwiftyJunnos <junnos.dev@gmail.com>
2024-05-08 15:06:15 +0900

    Fix device selection to select in order as: cuda -> mps -> cpu

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import logging
+from tqdm import tqdm
 
 import torch
 from transformers import BatchEncoding
@@ -13,10 +14,17 @@ from .const import (
 )
 from .network.roberta import fetch_RoBERTa_model
 from .tester import Tester
+from .trainer import Trainer
 
 _LOGGER = logging.getLogger(__name__)
 
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+EPOCHS = 5000
+
+device = torch.device(
+    "cuda" if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available()
+    else "cpu"
+)
 
 if __name__ == "__main__":
     tokenizer = QuestionTokenizer()
@@ -25,7 +33,7 @@ if __name__ == "__main__":
     valid_dataset = QuestionDataset(filename=VALID_SET, tokenizer=tokenizer)
     test_dataset = QuestionDataset(filename=TEST_SET, tokenizer=tokenizer)
 
-    train_loader = DataLoader(train_dataset, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
     valid_loader = DataLoader(valid_dataset, shuffle=False)
     test_loader = DataLoader(test_dataset, shuffle=False)
 
@@ -34,10 +42,17 @@ if __name__ == "__main__":
         num_labels=num_labels
     ).to(device)
 
+    trainer = Trainer(
+        model=model,
+        data_loader=train_loader,
+        device=device
+    )
+    for epoch in tqdm(range(EPOCHS)):
+        trainer.train(epoch=epoch)
+
     tester = Tester(
         model=model,
         data_loader=test_loader,
         device=device
     )
-
     tester.test()
